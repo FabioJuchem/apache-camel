48d6e56f88ad4684ac52048e6320d078d8ad07e9
FabioJuchem <fabiojuchem@gmail.com>
2020-07-27 21:09:06 -0300

    Saving

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -8,6 +9,23 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
 
+			@Override
+			public void configure() throws Exception {
+				from("file:pedidos?delay=5s&noop=true")
+						.split()
+							.xpath("/pedido/itens/item")
+						.marshal().xmljson()
+						.filter().xpath("/item/formato[text()='EBOOK']")
+						.log("${id}")
+						.log("${body}")
+						.setHeader("CamelFileName", simple("${file:name.noext}.xml"))
+				.to("file:saida");
+			}
+		});
+
+		context.start();
+		context.stop();
 	}	
 }
